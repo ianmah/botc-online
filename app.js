c8d4320d0052e380bcf800efd45b802a24824cd8
Ian Mah <ian.mah@hootsuite.com>
2020-04-03 14:12:58 -0700

    Add command router and newUser command

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const WebSocketServer = require('websocket').server;
 const http = require('http');
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
@@ -10,16 +11,32 @@ const httpServer = http.createServer(app);
 //initialize the WebSocket server instance
 const wsServer = new WebSocketServer({ httpServer });
 
+const users = []
+
 // WebSocket server
 wsServer.on('request', function(request) {
   var connection = request.accept(null, request.origin);
-  console.log('New websocket connection')
+  const uuid = uuidv4();
+  console.log('New websocket connection', uuid)
+  connection.send(JSON.stringify({
+    command: 'newConnection',
+    uuid
+  }))
 
   // This is the most important callback for us, we'll handle
   // all messages from users here.
   connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      // process WebSocket message
+    if (message.utf8Data) {
+      const data = JSON.parse(message.utf8Data) // {"name":"Ian","message":"Hi"}
+      console.log('New message:', data)
+
+      const command = data.command
+      switch (command) {
+        case 'newUser':
+          newUser(data)
+        default:
+          noCommand(data, connection)
+      }
     }
   });
 
@@ -32,3 +49,16 @@ wsServer.on('request', function(request) {
 httpServer.listen(process.env.PORT || 1337, () => {
   console.log(`Server started on port ${httpServer.address().port} :)`);
 });
+
+const newUser = (data) => {
+  const user = {
+    uuid: data.uuid,
+    name: data.name
+  }
+  
+  users.push(user)
+}
+
+const noCommand = ({command}, connection) => {
+  connection.send(`Command "${command}" not found`)
+}
